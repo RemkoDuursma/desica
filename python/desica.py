87dfd7b7c504f0f64e63d14eb3dacbe0ab3e67b6
mdekauwe <mdekauwe@gmail.com>
2017-11-23 16:50:02 +1100

    Added bones

---
+++ python/desica.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Desica model
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (23.11.2017)"
+__email__ = "mdekauwe@gmail.com"
+
+import pandas as pd
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+class Desica(object):
+
+    def __init__(self):
+
+        pass
+
+    def main(self):
+
+        pass
+    
+if __name__ == "__main__":
+
+    D = Desica()
+    D.main()
